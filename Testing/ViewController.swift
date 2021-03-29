3a9ea4587390a32c38c523bca595c19fd6ff9944
imarunreddy <arunreddy6008@gmail.com>
2021-03-29 15:51:55 +0530

    just printed

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("i had started")
+        
         // Do any additional setup after loading the view.
     }
 
